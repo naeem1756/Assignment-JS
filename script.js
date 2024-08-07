f76f78afe3962574bbe0182e8417ca07b19eaafd
naeem1756 <mohammadnaeem1756@gmail.com>
2024-08-08 04:10:13 +0500

    my java script assignment

---
+++ script.js
@@ -0,0 +1,83 @@
+function showErrorr() {
+    alert("Welcome to our website! We hope you have a great experience");
+}
+
+
+showErrorr();
+
+function showError() {
+    alert("Error! Please enter a valid password.");
+}
+
+
+showError();
+
+
+function showAlert() {
+   
+    const message = "Error!\nPlease enter a valid password.\nTry again.";
+
+    alert(message);
+}
+showAlert();
+
+console.log("This message is displayed in the console!");
+
+alert("This message is displayed as an alert!");
+
+
+var username = "Jhon swis";
+alert(username);
+
+var myName = "naeem";
+alert(myName);
+
+
+var message = "Hello World";
+alert(message);
+
+
+
+var studentName = "john";    
+        var studentAge = 18;               
+        var studentCourse = "science"; 
+        var studentSchool = "678 University";   
+
+        
+        alert("Student Name: " + studentName);          
+        alert("Student Age: " + studentAge);            
+        alert("Student Course: " + studentCourse);      
+        alert("Student School: " + studentSchool);   
+        
+        
+
+        var messagee = "PIZZA";
+        
+        var displayMessage = messagee + "\n" +
+                             messagee.slice(0, -1) + "\n" +
+                             messagee.slice(0, -2) + "\n" +
+                             messagee.slice(0, -3) + "\n" +
+                             messagee.slice(0, -4);
+    
+        alert(displayMessage);
+
+
+        var email = "example@example.com";
+
+        alert("My email addres is "+ email);
+
+        var book = "A smarter way to learn JavaScript";
+
+        alert("I am trying to learn from the book " + book);
+
+        var im = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+        alert(im);
+
+
+        var age = "i am 25 years old";
+        alert(age);
+
+        var visit = "You have visited this site 14 times."
+        alert(visit);
+
+        
